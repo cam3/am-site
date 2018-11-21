3104542412e1c11271a8dbcb2368d7c64a62813c
Cameron McWatters <cam3ron@gmail.com>
2018-11-21 15:41:02 -0600

    Added Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAa_BGKW3ZGM1x_CVmnCUJu-9gdqlTmTCI",
+    authDomain: "am-site-d1f10.firebaseapp.com",
+    databaseURL: "https://am-site-d1f10.firebaseio.com",
+    projectId: "am-site-d1f10",
+    storageBucket: "am-site-d1f10.appspot.com",
+    messagingSenderId: "216317551597"
+  }
 };
 
 /*
